e99f4f8248f8bc4cde9bc13b8fbeeddc66bffed4
Federico Pernas <federico.pernas@endava.com>
2021-11-16 09:18:38 -0300

    Add Bean for GraphQL handle lazy loading

--- backend/src/main/java/com/dev/week/devweek/DevweekApplication.java
+++ backend/src/main/java/com/dev/week/devweek/DevweekApplication.java
@@ -1,7 +1,11 @@
 package com.dev.week.devweek;
 
+import javax.servlet.Filter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
 
 @SpringBootApplication
 public class DevweekApplication {
@@ -10,4 +14,14 @@ public class DevweekApplication {
 		SpringApplication.run(DevweekApplication.class, args);
 	}
 
+	/**
+	 * This bean is added so GraphQL knows
+	 * how to deal with lazy loadings during
+	 * execution (character's collections)
+	 * 
+	*/
+	@Bean
+	public Filter OpenFilter() {
+		return new OpenEntityManagerInViewFilter();
+	}
 }
