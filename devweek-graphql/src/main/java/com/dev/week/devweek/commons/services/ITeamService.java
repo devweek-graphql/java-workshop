52a99ec71ce3a7b963d1cdefd3eae31f9393563f
Federico Pernas <federico.pernas@endava.com>
2021-11-19 10:05:48 -0300

    adjustments

--- devweek-graphql/src/main/java/com/dev/week/devweek/commons/services/ITeamService.java
+++ devweek-graphql/src/main/java/com/dev/week/devweek/commons/services/ITeamService.java
@@ -3,11 +3,8 @@ package com.dev.week.devweek.commons.services;
 import java.util.List;
 
 import com.dev.week.devweek.commons.model.Team;
-import com.dev.week.devweek.graphql.models.AddTeamPayload;
 
 public interface ITeamService {
     
     List<Team> getAllTeams();
-
-    Team addNewTeam(AddTeamPayload payload);
 }
--- devweek-graphql/src/main/java/com/dev/week/devweek/commons/services/TeamService.java
+++ devweek-graphql/src/main/java/com/dev/week/devweek/commons/services/TeamService.java
@@ -4,10 +4,8 @@ import java.util.List;
 
 import com.dev.week.devweek.commons.model.Team;
 import com.dev.week.devweek.commons.repositories.ITeamRepository;
-import com.dev.week.devweek.graphql.models.AddTeamPayload;
 
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 
 @Service
 public class TeamService implements ITeamService {
@@ -21,18 +19,6 @@ public class TeamService implements ITeamService {
     @Override
     public List<Team> getAllTeams() {
         return this.teamRepository.findAll();
-    }
-
-    @Override
-    public Team addNewTeam(AddTeamPayload payload) {
-        if (StringUtils.hasText(payload.getName())) {
-            Team team = new Team();
-            team.setName(payload.getName());
-            team.setDescription(payload.getDescription());
-            return this.teamRepository.saveAndFlush(team);
-        }
-
-        return null;
     }
     
 }
